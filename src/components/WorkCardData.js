340b63635fa0f6942a96a0ad99290582b93cdc39
Jo <josiahng.han@gmail.com>
2025-09-13 13:28:18 +0100

    update project

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,21 +1,28 @@
 import portfolio from "../assets/portfolio.png";
 import Profile from "../assets/Profile.png";
+import TodoList from "../assets/TodoList.png";
 
 const ProjectCardData = [
-    {
-        imgsrc: portfolio,
-        title: "Portfolio Website",
-        text: "This portfolio website is built with React. I started it as a small project to have a place where I could share a bit about myself and the things I work on. Along the way, it also became a space for me to experiment with new tools and technologies, learn by doing, and gradually shape it into something that grows with me.",
-        view: "/",
-        source: "https://github.com/Jng1012/my-website"
-    },
-    {
-        imgsrc: Profile,
-        title: "Financial Expenses Tracker",
-        text: "This is a container to add textThis is a container to add textThis is a container to add textThis is a container to add textThis is a container to add textThis is a container to add textThis is a container to add textThis is a container to add textThis is aThis is a container to add textThis is a container to add textThis is a container to add textThis is a",
-        view: "<sample url>"
-    }
-
+  {
+    imgsrc: portfolio,
+    title: "Portfolio Website",
+    text: "This portfolio website is built with React. I started it as a small project to have a place where I could share a bit about myself and the things I work on. Along the way, it also became a space for me to experiment with new tools and technologies, learn by doing, and gradually shape it into something that grows with me.",
+    view: "/",
+    source: "https://github.com/Jng1012/my-website",
+  },
+  {
+    imgsrc: TodoList,
+    title: "Todo-List",
+    text: "This Todo-list app is built with the MERN stack. I started it as a small project to manage tasks and understand full-stack development after learning about frontend and React. It became a space to explore new tools, experiment with React and MongoDB, leverage AI for learning, and gradually build confidence in creating complete applications.",
+    view: "https://todo-list-9h0h.onrender.com/",
+    source: "https://github.com/Jng1012/todo-list"
+  },
+  {
+    imgsrc: Profile,
+    title: "Expenses Tracker",
+    text: "This is a container to add textThis is a container to add textThis is a container to add textThis is a container to add textThis is a container to add textThis is a container to add textThis is a container to add textThis is a container to add textThis is aThis is a container to add textThis is a container to add textThis is a container to add textThis is a",
+    view: "<sample url>",
+  },
 ];
 
 export default ProjectCardData;
